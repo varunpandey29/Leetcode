934041ef00ec9ec75ace3c7c34d853f9fbfc8f4e
Varun Pandey <98257202+varunpandey29@users.noreply.github.com>
2023-07-28 11:20:59 +0530

    Time: 3 ms (70.47%) | Memory: 7.2 MB (95.12%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -1,18 +1,28 @@
 class Solution {
 public:
+    long long dp[22][22][2] ;
+
     int maxScore(vector<int>& nums, int i, int j, int chance){
         if(i>j)
             return 0;
+
+        if(dp[i][j][chance] != -1)
+        {
+            return dp[i][j][chance];
+        }
+
         if(chance==0){
-            return max(nums[i]+maxScore(nums, i+1, j, 1), nums[j]+maxScore(nums, i, j-1, 1));
+            return dp[i][j][chance] =max(nums[i]+maxScore(nums, i+1, j, 1), nums[j]+maxScore(nums, i, j-1, 1));
         }
         else
-            return min(maxScore(nums, i+1, j, 0), maxScore(nums,i, j-1, 0));
+            return dp[i][j][chance] = min(maxScore(nums, i+1, j, 0), maxScore(nums,i, j-1, 0));
     }
     bool PredictTheWinner(vector<int>& nums) {
         long long two = 0;
         long long total = 0;
         int n = nums.size();
+
+        memset(dp, -1, sizeof(dp));
 
         for(int i = 0; i<n; i++)
         {
