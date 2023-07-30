4fd9bf971f0f954e026eacdba40a1dbc7e7e7c1e
Varun Pandey <98257202+varunpandey29@users.noreply.github.com>
2023-07-30 23:56:02 +0530

    list-with-random-pointer.cpp

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
@@ -1,18 +1,3 @@
-/*
-// Definition for a Node.
-class Node {
-public:
-    int val;
-    Node* next;
-    Node* random;
-    
-    Node(int _val) {
-        val = _val;
-        next = NULL;
-        random = NULL;
-    }
-};
-*/
 
 class Solution {
 private:
